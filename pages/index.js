43441dde19b692163e16605d7b5533644ae38289
Yusuke0018 <youyou00181002@gmail.com>
2025-06-15 23:06:43 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-
 export default function Home() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>動作確認OK</h1>
-      <p>エラーが解決しました</p>
+    <div>
+      <h1>テスト成功</h1>
     </div>
-  );
+  )
 }
